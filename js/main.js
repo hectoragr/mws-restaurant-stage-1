4a1f378925081a72205f53a593593c7f9a1720d1
Héctor Gómez Reyes <hector.agr@gmail.com>
2018-12-17 17:37:02 -0800

    Fix: Udacity Feedback Fixed Aria roles for map and tabindex Changed headings to only one main h1 and multiple h2 or h3

--- js/main.js
+++ js/main.js
@@ -154,7 +154,7 @@ createRestaurantHTML = (restaurant) => {
   image.sizes = DBHelper.imageSizesForRestaurant();
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
